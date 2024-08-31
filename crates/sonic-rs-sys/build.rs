2430115c2ade099862ae61da99284ccb0fb87f84
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-31 17:44:52 +0300

    fix: copy sonic to dst

--- crates/sonic-rs-sys/build.rs
+++ crates/sonic-rs-sys/build.rs
@@ -1,7 +1,35 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+fn copy_folder(src: &Path, dst: &Path) {
+    std::fs::create_dir_all(dst).expect("Failed to create dst directory");
+    if cfg!(unix) {
+        std::process::Command::new("cp")
+            .arg("-rf")
+            .arg(src)
+            .arg(dst.parent().unwrap())
+            .status()
+            .expect("Failed to execute cp command");
+    }
+
+    if cfg!(windows) {
+        std::process::Command::new("robocopy.exe")
+            .arg("/e")
+            .arg(src)
+            .arg(dst)
+            .status()
+            .expect("Failed to execute robocopy command");
+    }
+}
 
 fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("Failed to get CARGO_MANIFEST_DIR");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let sonic_src = Path::new(&manifest_dir).join("sonic");
+    let sonic_dst = out_dir.join("sonic");
+    copy_folder(&sonic_src, &sonic_dst);
+
     println!("cargo:rustc-link-lib=static=libsonic");
     println!("cargo:rerun-if-changed=./sonic/sonic.h");
     println!("cargo:rerun-if-changed=./sonic/sonic.c");
