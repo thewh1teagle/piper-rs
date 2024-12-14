558a9d5452c191a76c587875ba8f43bedf74bb46
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-12-14 23:51:38 +0200

    feat/piper-rs-cli (#6)
    feat: piper-rs-cli

---
+++ crates/piper-rs-cli/src/main.rs
@@ -0,0 +1,89 @@
+/*
+wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/hfc_female/medium/en_US-hfc_female-medium.onnx
+wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/hfc_female/medium/en_US-hfc_female-medium.onnx.json
+
+cargo install piper-rs-cli
+piper-rs-cli en_US-hfc_female-medium.onnx.json "Hello from piper-rs-cli!"
+*/
+
+use clap::Parser;
+use eyre::{bail, Result};
+use piper_rs::synth::PiperSpeechSynthesizer;
+use rodio::buffer::SamplesBuffer;
+use std::{
+    path::{Path, PathBuf},
+    time::Instant,
+};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to JSON config
+    config: String,
+
+    /// Text to create
+    text: String,
+
+    /// Wav file path to create
+    /// Optional, it will play it directly if not provided.
+    #[arg(short, long)]
+    out: Option<String>,
+
+    /// Path to Model
+    #[arg(short, long)]
+    model: Option<String>,
+
+    /// Path to Model
+    #[arg(long)]
+    speaker_id: Option<i64>,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+    tracing::debug!("hi");
+    let args = Args::parse();
+    let model = args
+        .model
+        .map(|m| PathBuf::from(m))
+        .unwrap_or_else(|| PathBuf::from(&args.config.replace(".onnx.json", ".onnx")));
+    if !model.exists() {
+        bail!("Model not found at {}", model.display())
+    }
+    let model = piper_rs::from_config_path(Path::new(&args.config)).unwrap();
+    if let Some(sid) = args.speaker_id {
+        model.set_speaker(sid);
+    }
+    let synth = PiperSpeechSynthesizer::new(model).unwrap();
+
+    if let Some(path) = args.out {
+        // Save to file
+        let start_t = Instant::now();
+        synth.synthesize_to_file(&PathBuf::from(&path), args.text, None)?;
+        tracing::debug!("Took {} seconds", start_t.elapsed().as_secs());
+        println!("Created {}", path);
+    } else {
+        // Play directly in memory
+        let start_t = Instant::now();
+        let audio = synth.synthesize_parallel(args.text, None).unwrap();
+        tracing::debug!("Took {} seconds", start_t.elapsed().as_secs());
+        let mut samples: Vec<f32> = Vec::new();
+        for result in audio {
+            samples.append(&mut result.unwrap().into_vec());
+        }
+
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = rodio::Sink::try_new(&handle).unwrap();
+
+        let buf = SamplesBuffer::new(1, 22050, samples);
+        sink.append(buf);
+
+        println!("Playing...");
+        sink.sleep_until_end();
+    }
+    Ok(())
+}
